a15d473880ba02723747df66f6f62c4c59d97cba
Daniel Lee <dan0196@pm.me>
2022-01-03 22:41:23 +0900

    finished gatsby tutorial part 3

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,12 +2,15 @@
 import * as React from 'react'
 import { Link } from "gatsby";
 import Layout from '../components/layout';
+import { StaticImage } from 'gatsby-plugin-image';
 
 // Step 2: Define your component
 const IndexPage = () => {
   return (
     <Layout pageTitle="Home">
       <p>I'm making this by following the Gatsby Tutorial.</p>
+      <StaticImage alt="Astronaut logo" src="../images/gatsby-astronaut.png"
+      />
     </Layout>
   )
 }
